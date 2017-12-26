efc89b3af89b095f0433f26f0f461cc07267ae19
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 19:52:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -103,9 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 programmer_hash[:alan_kay][:known_for] = "GUI"
-# alan_kay_is_known_for
 programmer_hash
-# [:alan_kay][:known_for]).to eq("GUI")
 end
 
 def adding_to_dennis
