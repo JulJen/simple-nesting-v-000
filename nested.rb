dd6bbc73aece2431121f3daa7ca7ea826ebc81e4
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 20:05:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,9 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    #  it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do
-    #    expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-# programmer_hash[:dennis_ritchie][:languages]
+
 programmer_hash
 end
